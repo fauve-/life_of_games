31e751904472be8dc69e18ac880cd2624a3c31bb
fauve- <crawfordpm@mymail.vcu.edu>
2013-06-16 20:57:03 -0400

    overzealous editing

--- gol.c
+++ gol.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+/*
+ */
 
 
 //grabbed from the sweet stanford bithacks
@@ -14,16 +16,16 @@ int  NumberOfSetBits(int i){
 }
 
 #define GETWORLD(i) world[i]
-const long offsetTop = 0x380000000;  //0x7 << 32;
-const long offsetMid = 0x280000000; //0x5 << 32;
-const long offsetBot = 0x380000000;// 0x7 << 32;
 
 unsigned int * turn(unsigned int* world, int side){
   int othersideOffset, thisOffset;
   unsigned int i, j;
   //explicit for now, abstract away later
-  thisOffset = side == 0 ? 0 : 32;
+  thisOffset = side == 0? 0 : 32;
   othersideOffset = side == 0 ? 32: 0;
+  const long offsetTop = 0x380000000;  //0x7 << 32;
+  const long offsetMid = 0x280000000; //0x5 << 32;
+  const long offsetBot = 0x380000000;// 0x7 << 32;
   for( i = 0; i < 32; i++){
     for(j = 1; j < 33; j++){
       int counter = 0;
@@ -39,29 +41,29 @@ unsigned int * turn(unsigned int* world, int side){
         int mask = GETWORLD(i + 1 + thisOffset) & (offsetBot >> j);
         counter+=NumberOfSetBits(mask);
       }
-      //printf("%i ",counter);
+      //      printf("%i ",counter);
       if(counter == 3 || 
          ((counter == 2) && (GETWORLD(i + thisOffset) & (1 << j - 1 )))){
-        (GETWORLD(i + othersideOffset)) |=  (1 << (32 - j));
+        (GETWORLD(i + othersideOffset)) |=  (1 << (j - 1));
       }else{
-        (GETWORLD(i + othersideOffset)) &= ~(1 << (32 - j));
+        (GETWORLD(i + othersideOffset)) &= ~(1 << (j - 1));
       }
     }
-    //    printf("\n");
+    //printf("\n");
   }
-  printf("FRAME\n");
+	printf("FRAME\n");
   return world;
 }
 
-const char alive = '#';
-const char dead = '.';
+  const char alive = '#';
+  const char dead = '.';
 
 void printIt(unsigned int *world, int side){
   int i,j;
-  //  char* outerstring[32];
+  char* outerstring[32];
   int thisOffset = side == 1? 32 : 0;
   for(i = 0; i < 32;i++){
-    char innerstring[33];    
+    char innerstring[32];    
     for(j = 0; j < 32; j++){
       if(GETWORLD(i + thisOffset) & (1 << j)){
         innerstring[j] = alive;
@@ -69,16 +71,19 @@ void printIt(unsigned int *world, int side){
         innerstring[j] = dead;
       }
     }
-    innerstring[32] = '\000';
-    printf("%s\n",innerstring);
+    outerstring[i] = (char*) malloc(sizeof(char) * 32);
+    strncpy(outerstring[i], innerstring, 32);
+  }
+  for(i = 0; i < 32; i++){
+    printf("%s\n", outerstring[i]);
   }
 }
 
-unsigned int* seed_world(unsigned int *world, int life_chance){
+unsigned int* seed_world(unsigned int *world){
   int i,j;
   for(i = 0; i < 32; i++){
     for(j = 0; j < 32; j++){
-      if(rand() % (100 - life_chance) == 0){
+      if(rand() % 20 == 0){
         world[i] |= (1 << j);
       }
     }
@@ -101,7 +106,6 @@ unsigned int* seed_world_test(unsigned int *world){
 void gameloop(unsigned int *world, int duration){
   int i;
   duration = duration == -1? ~0 : duration;
-  printIt(world,0);
   for(i = 0; i < duration; i++){
     world = turn(world, i % 2);
     printIt(world, (i + 1) %2);
@@ -109,13 +113,14 @@ void gameloop(unsigned int *world, int duration){
 
 }
 
-int main(int argc, char** argv){
+
+int main(){
   unsigned int *world;
-  int i, iterations, alive_percent;
-  int *args;
-  srand(time(NULL));  //  printf("%i \t %i",args[0], args[1]);
-  gameloop(world,2);
-  free(world);
-  return 0;
+  int i;
+  //  turn(world, 1);
+  srand(time(NULL));
+  world = (unsigned int *) calloc(32 * 4,sizeof(unsigned int));
+  world = seed_world(world);
+  gameloop(world,1000000);
 }
 
